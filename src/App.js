27e3e40aaf61554672b30b4223b8e3534364f8ab
Joan Yassi <joan@tray.io>
2020-08-05 05:34:08 +0100

    Added the components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,78 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+import { Typography } from "@material-ui/core";
+import wordsToNumbers from "words-to-numbers";
+
+import { NewsCards, Modal } from "./components";
+import useStyles from "./styles";
 
 const App = () => {
-  return <div>Hello from app</div>;
+  const classes = useStyles();
+  const [activeArticle, setActiveArticle] = useState(0);
+  const [newsArticles, setNewsArticles] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+  useEffect(() => {
+    alanBtn({
+      key: process.env.REACT_APP_ALAN_API_KEY,
+      onCommand: ({ command, articles, number }) => {
+        if (command === "newHeadlines") {
+          setNewsArticles(articles);
+          setActiveArticle(-1);
+        } else if (command === "instructions") {
+          setIsOpen(true);
+        } else if (command === "highlight") {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === "open") {
+          const parsedNumber =
+            number.length > 2
+              ? wordsToNumbers(number, { fuzzy: true })
+              : number;
+          const article = articles[parsedNumber - 1];
+
+          if (parsedNumber > 20) {
+            alanBtn().playText("Please try that again...");
+          } else if (article) {
+            window.open(article.url, "_blank");
+            alanBtn().playText("Opening...");
+          } else {
+            alanBtn().playText("Please try that again...");
+          }
+        }
+      },
+    });
+  });
+
+  return (
+    <div>
+      <div className={classes.logoContainer}>
+        {newsArticles.length ? (
+          <div className={classes.infoContainer}>
+            <div className={classes.card}>
+              <Typography variant="h5" component="h2">
+                Try saying: <br />
+                <br />
+                Open article number [4]
+              </Typography>
+            </div>
+            <div className={classes.card}>
+              <Typography variant="h5" component="h2">
+                Try saying: <br />
+                <br />
+                Go back
+              </Typography>
+            </div>
+          </div>
+        ) : null}
+        <img
+          src="https://alan.app/voice/images/previews/preview.jpg"
+          className={classes.alanLogo}
+          alt="logo"
+        />
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+      <Modal isOpen={isOpen} setIsOpen={setIsOpen} />
+    </div>
+  );
 };
 
 export default App;
--- src/component/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ComponentTest = () => {
-  return <div>Hello from component test</div>;
-};
-
-export default ComponentTest;
